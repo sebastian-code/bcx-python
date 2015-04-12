5a8940214343284ef0324556ce72e7d659831f59
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-04-12 12:58:11 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+La idea del proyecto es crear un cliente que permita exportar informacion de los diferentes proyectos de BaseCamp hacia un archivo manejable con Excel, para crear pequeños tableros de control y reportes personalizados, a partir del a informacion existente.
+
+Archivo en desarrollo
---
+++ bcx_lib.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+"""
+Created on 2015-04-12
+@note: Compatible con Python3
+@summary: Cliente para extraccion de informacion de una cuenta de BaseCamp
+                   Este cliente unicamente ejecuta accesos de lectura, y esta
+                   creado con la finalidad de extraer toda la informacion
+                   disponible para la cuenta suministrada, con fines de crear
+                   reportes directamente en excel.
+@author: Sebastian Reyes Espinosa
+@contact: sebaslander@gmail.com
+"""
+
+__author__ = 'Sebastian Reyes Espinosa'
+__email__ = 'sebaslander@gmail.com'
+__creation_date__ = '2015-04-12'
+
+
